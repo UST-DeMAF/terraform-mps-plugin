7c759a1494f6e0e4fe098505ab33f2c57617196d
Arikuma <st162924@stud.uni-stuttgart.de>
2024-08-04 16:48:49 +0200

    Added connection check for plugin registration

--- src/main/java/ust/tad/terraformmpsplugin/registration/PluginRegistrationRunner.java
+++ src/main/java/ust/tad/terraformmpsplugin/registration/PluginRegistrationRunner.java
@@ -23,6 +23,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import ust.tad.terraformmpsplugin.analysistask.AnalysisTaskReceiver;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 /**
  * Runner that is executed at application startup to register this plugin at the Analysis Manager.
  */
@@ -44,6 +48,9 @@ public class PluginRegistrationRunner implements ApplicationRunner{
     @Autowired
     private AnalysisTaskReceiver analysisTaskReceiver;
 
+    @Value("${analysis-manager.plugin-registration.url}")
+    private String pluginRegistrationURI;
+
     @Value("${plugin.technology}")
     private String pluginTechnology;
 
@@ -54,10 +61,22 @@ public class PluginRegistrationRunner implements ApplicationRunner{
     private String responseExchangeName;
 
     @Override
-    public void run(ApplicationArguments args) throws JsonProcessingException {
+    public void run(ApplicationArguments args) throws JsonProcessingException, InterruptedException {
 
         LOG.info("Registering Plugin");
 
+        String host = pluginRegistrationURI.split(":")[1].replace("/","");
+        int port = Integer.parseInt(pluginRegistrationURI.split(":")[2].split("/")[0]);
+        int maxAttempts = 20;
+
+        for (int attempt = 0; attempt < maxAttempts; attempt++)
+        {
+            if(isServiceReachable(host, port, 5000, attempt + 1, maxAttempts))
+            {
+                break;
+            }
+            Thread.sleep(2000);
+        }
         String body = createPluginRegistrationBody();
 
         PluginRegistrationResponse response = pluginRegistrationApiClient.post()
@@ -95,6 +114,19 @@ public class PluginRegistrationRunner implements ApplicationRunner{
         listener.start();
     
         return listener;
+    }
+
+    public static boolean isServiceReachable(String hostNameOrIP, int port, int timeout, int attempt, int maxAttempts) {
+        try (Socket socket = new Socket()) {
+            // Attempt to connect to the host and port within the given timeout
+            socket.connect(new InetSocketAddress(hostNameOrIP, port), timeout);
+            LOG.info("Service " + hostNameOrIP + " is reachable. Start registration." );
+            return true;
+        } catch (IOException e) {
+            // Connection failed or timed out
+            LOG.info("Service " + hostNameOrIP + " isn't reachable. Attempt (" + attempt + "/" + maxAttempts +")");
+            return false;
+        }
     }
     
 }
