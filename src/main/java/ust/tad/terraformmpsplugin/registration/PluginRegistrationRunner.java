584f9d314c3ee397ef67e54b4c376182b3daad10
Arikuma97 <44363150+Arikuma97@users.noreply.github.com>
2024-08-11 18:50:32 +0200

    Added connection check for plugin registration (#2)
    * Added connection check for plugin registration
    
    * Adjusted buildAndPushImage.yml so dev branch is also an image
    
    * Added JavaDoc & refactored Method

--- src/main/java/ust/tad/terraformmpsplugin/registration/PluginRegistrationRunner.java
+++ src/main/java/ust/tad/terraformmpsplugin/registration/PluginRegistrationRunner.java
@@ -23,6 +23,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import ust.tad.terraformmpsplugin.analysistask.AnalysisTaskReceiver;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 /**
  * Runner that is executed at application startup to register this plugin at the Analysis Manager.
  */
@@ -44,6 +48,9 @@ public class PluginRegistrationRunner implements ApplicationRunner{
     @Autowired
     private AnalysisTaskReceiver analysisTaskReceiver;
 
+    @Value("${analysis-manager.plugin-registration.url}")
+    private String pluginRegistrationURI;
+
     @Value("${plugin.technology}")
     private String pluginTechnology;
 
@@ -54,9 +61,11 @@ public class PluginRegistrationRunner implements ApplicationRunner{
     private String responseExchangeName;
 
     @Override
-    public void run(ApplicationArguments args) throws JsonProcessingException {
+    public void run(ApplicationArguments args) throws JsonProcessingException, InterruptedException {
 
         LOG.info("Registering Plugin");
+
+        connectionAttempt();
 
         String body = createPluginRegistrationBody();
 
@@ -80,6 +89,25 @@ public class PluginRegistrationRunner implements ApplicationRunner{
             () -> new FanoutExchange(response.getResponseExchangeName(), true, false));
     }
 
+    /**
+     * Try to reach the service 20 times (maxAttempts).
+     * @throws InterruptedException
+     */
+    private void connectionAttempt() throws InterruptedException {
+        String host = pluginRegistrationURI.split(":")[1].replace("/","");
+        int port = Integer.parseInt(pluginRegistrationURI.split(":")[2].split("/")[0]);
+        int maxAttempts = 20;
+
+        for (int attempt = 0; attempt < maxAttempts; attempt++)
+        {
+            if(isServiceReachable(host, port, 5000, attempt + 1, maxAttempts))
+            {
+                break;
+            }
+            Thread.sleep(2000);
+        }
+    }
+
     private String createPluginRegistrationBody() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode plugin = mapper.createObjectNode();
@@ -95,6 +123,28 @@ public class PluginRegistrationRunner implements ApplicationRunner{
         listener.start();
     
         return listener;
+    }
+
+    /**
+     * Checks if Service is reachable. If so, it returns true so the plugin can start connecting otherwise it returns false.
+     * @param hostNameOrIP
+     * @param port
+     * @param timeout
+     * @param attempt
+     * @param maxAttempts
+     * @return boolean
+     */
+    public static boolean isServiceReachable(String hostNameOrIP, int port, int timeout, int attempt, int maxAttempts) {
+        try (Socket socket = new Socket()) {
+            // Attempt to connect to the host and port within the given timeout
+            socket.connect(new InetSocketAddress(hostNameOrIP, port), timeout);
+            LOG.info("Service " + hostNameOrIP + " is reachable. Start registration." );
+            return true;
+        } catch (IOException e) {
+            // Connection failed or timed out
+            LOG.info("Service " + hostNameOrIP + " isn't reachable. Attempt (" + attempt + "/" + maxAttempts +")");
+            return false;
+        }
     }
     
 }
