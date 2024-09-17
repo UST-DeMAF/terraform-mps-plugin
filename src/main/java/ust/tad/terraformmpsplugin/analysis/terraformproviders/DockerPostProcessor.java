c871e92b151d7468a2efb9c83b71bc21a28d1ca8
Elias Müller <elias_benedikt.mueller@porsche.de>
2024-09-17 10:16:12 +0200

    fix(ES-53): fix docker-post processor

--- src/main/java/ust/tad/terraformmpsplugin/analysis/terraformproviders/DockerPostProcessor.java
+++ src/main/java/ust/tad/terraformmpsplugin/analysis/terraformproviders/DockerPostProcessor.java
@@ -1,78 +1,55 @@
 package ust.tad.terraformmpsplugin.analysis.terraformproviders;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
 import ust.tad.terraformmpsplugin.models.tadm.*;
 
-import javax.sound.sampled.Port;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class DockerPostProcessor {
-    private final String PROVIDERNAME = "docker_container";
+  private final String PROVIDERNAME = "docker_container";
 
-    /**
-     * Tests if this Post-Processor is applicable for the given technology-agnostic deployment
-     * model.
-     *
-     * @param tadm the technology-agnostic deployment model to test.
-     * @return true if applicable, false otherwise.
-     */
-    public boolean isPostProcessorApplicable(TechnologyAgnosticDeploymentModel tadm) {
-        for (ComponentType componentType : tadm.getComponentTypes()) {
-            if (componentType.getName().startsWith(PROVIDERNAME)) {
-                return true;
-            }
-        }
-        return false;
+  /**
+   * Tests if this Post-Processor is applicable for the given technology-agnostic deployment model.
+   *
+   * @param tadm the technology-agnostic deployment model to test.
+   * @return true if applicable, false otherwise.
+   */
+  public boolean isPostProcessorApplicable(TechnologyAgnosticDeploymentModel tadm) {
+    for (ComponentType componentType : tadm.getComponentTypes()) {
+      if (componentType.getName().startsWith(PROVIDERNAME)) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    /**
-     * Runs the post-processing steps for this provider.
-     *
-     * @param tadm the technology-agnostic deployment model to post-process.
-     * @return the modified technology-agnostic deployment model.
-     * @throws PostProcessorFailedException if the post-processing fails.
-     */
-    public TechnologyAgnosticDeploymentModel runPostProcessor(TechnologyAgnosticDeploymentModel tadm) throws PostProcessorFailedException, InvalidPropertyValueException {
-        createContainerRuntime(tadm);
-        return tadm;
-    }
+  /**
+   * Runs the post-processing steps for this provider.
+   *
+   * @param tadm the technology-agnostic deployment model to post-process.
+   * @return the modified technology-agnostic deployment model.
+   * @throws PostProcessorFailedException if the post-processing fails.
+   */
+  public TechnologyAgnosticDeploymentModel runPostProcessor(TechnologyAgnosticDeploymentModel tadm)
+      throws PostProcessorFailedException, InvalidPropertyValueException {
+    createContainerRuntime(tadm);
+    return tadm;
+  }
 
-    /**
-     * Creates and adds the containeruntime object and type to tadm
-     *
-     * @param tadm
-     */
-    private void createContainerRuntime(TechnologyAgnosticDeploymentModel tadm) throws InvalidPropertyValueException {
-        Property name = new Property();
-        name.setKey("name");
-        name.setType(PropertyType.STRING);
-        name.setRequired(false);
-        Property version = new Property();
-        version.setKey("version");
-        version.setType(PropertyType.STRING);
-        version.setRequired(false);
-        List<Property> properties = new ArrayList<>();
-        properties.add(name);
-        properties.add(version);
+  /**
+   * Creates and adds the containeruntime object and type to tadm
+   *
+   * @param tadm the tadm to be modified
+   */
+  private void createContainerRuntime(TechnologyAgnosticDeploymentModel tadm) throws InvalidPropertyValueException {
+    Property name = new Property("name", PropertyType.STRING, false, null, Confidence.SUSPECTED);
+    Property version = new Property("version", PropertyType.STRING, false, null, null);
+    ComponentType containerRuntimeType = new ComponentType("container_runtime", null, List.of(name, version), null, null);
+    tadm.getComponentTypes().add(containerRuntimeType);
 
-        ComponentType containerRuntimeType = new ComponentType();
-        containerRuntimeType.setName("container_runtime");
-        containerRuntimeType.setProperties(null);
-        tadm.getComponentTypes().add(containerRuntimeType);
-
-        Component containerRuntime = new Component();
-        containerRuntime.setType(containerRuntimeType);
-        containerRuntime.setName("default-container-runtime");
-        Property runtimeName = new Property();
-        runtimeName.setKey("name");
-        runtimeName.setValue("Docker Desktop");
-        List<Property> runtimeProperties = new ArrayList<>();
-        runtimeProperties.add(runtimeName);
-        containerRuntime.setProperties(runtimeProperties);
-        tadm.getComponents().add(containerRuntime);
-    }
+    Property runtimeName = new Property("name", PropertyType.STRING, false, "Docker Desktop", Confidence.SUSPECTED);
+    Property runtimeVersion = new Property("version", PropertyType.STRING, false, "4.34.2", Confidence.SUSPECTED);
+    Component containerRuntime = new Component("default-container-runtime", null, List.of(runtimeName, runtimeVersion), null, containerRuntimeType, null, Confidence.SUSPECTED);
+    tadm.getComponents().add(containerRuntime);
+  }
 }
